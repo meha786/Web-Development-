8a44a7fd009fb80bc9b50b4c44c50bf5fd8aae07
meha786 <verma.me@husky.neu.edu>
2016-12-29 20:36:17 -0500

    Javascript first basic exercise

---
+++ Java Script learning/Exercise 1/script.js
@@ -0,0 +1,12 @@
+//alert("hello from the js file");
+
+var firstName = prompt("What is your first name");
+
+var lastName = prompt("What is your last name");
+
+var age = prompt("what is your age");
+
+alert("Nice to meet you " + (firstName + lastName));
+
+console.log("Your full name is " + firstName +" " +lastName);
+console.log("your age is " + age);
