8359fcf5414f604904b53b2bdf5c04116d225610
meha786 <verma.me@husky.neu.edu>
2017-01-03 20:09:33 -0800

    While Loop Learning

---
+++ Java Script learning/While Loops Learning/problem.js
@@ -0,0 +1,49 @@
+alert("javascript connected");
+
+var count = -10;
+
+while(count <= 19)
+{
+	console.log(count);
+	count++;
+}
+
+console.log("==========================");
+var count1 = 10;
+
+while(count1 <= 40)
+{
+	if(count1%2===0)
+	{
+		console.log(count1);
+	}
+	
+	count1++;
+}
+
+
+console.log("==========================");
+var count2 = 300;
+
+while(count2 <= 333)
+{
+	if(count2%2!==0)
+	{
+		console.log(count2);
+	}
+	
+	count2++;
+}
+
+console.log("==========================");
+var count3 = 5;
+
+while(count3 <= 50)
+{
+	if(count3%3===0 && count3%5===0)
+	{
+		console.log(count3);
+	}
+	
+	count3++;
+}
