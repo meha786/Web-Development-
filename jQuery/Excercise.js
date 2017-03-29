012a373c65c1e0f196c518e8d70408a0835d1c63
meha786 <verma.me@husky.neu.edu>
2017-03-29 15:26:45 -0700

    Excercise on  jQuery

---
+++ jQuery/Excercise.js
@@ -0,0 +1,14 @@
+if (jQuery) {
+
+	alert("jQuery loaded");
+} else{
+	alert("Not loaded");
+}
+
+$("div").css("background","purple");
+
+$(".highlight").css("width"," 200px");
+
+$("#third").css("border","2px solid orange");
+
+$("div:first-of-type").css("color","pink");
